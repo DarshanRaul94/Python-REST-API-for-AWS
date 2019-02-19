7b2784d3afd98fbc7cc4c6c36323c6c4ab973088
Darshan Raul <dar.raul@gmail.com>
2019-02-19 23:48:13 +0530

    Update aws.py

--- aws.py
+++ aws.py
@@ -2,7 +2,24 @@ from bottle import route, run
 import boto3
 import json
 s3=boto3.client('s3')
+from bottle import hook, route, response
 
+_allow_origin = '*'
+_allow_methods = 'PUT, GET, POST, DELETE, OPTIONS'
+_allow_headers = 'Authorization, Origin, Accept, Content-Type, X-Requested-With'
+
+@hook('after_request')
+def enable_cors():
+    '''Add headers to enable CORS'''
+
+    response.headers['Access-Control-Allow-Origin'] = _allow_origin
+    response.headers['Access-Control-Allow-Methods'] = _allow_methods
+    response.headers['Access-Control-Allow-Headers'] = _allow_headers
+
+@route('/', method = 'OPTIONS')
+@route('/<path:path>', method = 'OPTIONS')
+def options_handler(path = None):
+    return
 @route('/buckets/all')
 def getbuckets():
     buckets=s3.list_buckets()
