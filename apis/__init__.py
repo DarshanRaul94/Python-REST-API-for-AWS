6b432669e082be0ebad2a821d4c1a4487ac4abfa
darshan <dar.raul@gmail.com>
2019-03-02 15:04:55 +0530

    basic namespaces setup

---
+++ apis/__init__.py
@@ -0,0 +1,15 @@
+from flask_restplus import Api
+
+from .s3Namespace import api as s3
+from .Ec2Namespace import api as ec2
+
+
+api = Api(
+    title='AWS API',
+    version='1.0',
+    description='Use this APi to interact with your AWS services',
+   
+)
+
+api.add_namespace(s3, path='/s3')
+api.add_namespace(ec2, path='/ec2')
