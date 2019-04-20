ee0dd0f02affdd8695b61e92ee530d49e81fbc96
Darshan Raul <dar.raul@gmail.com>
2019-04-20 22:05:06 +0530

    Update __init__.py

--- apis_fire/__init__.py
+++ apis_fire/__init__.py
@@ -1,12 +1,14 @@
-from flask_restplus import Api
+from flask_restplus import Api ### import flask package  
 
+
+##########importing all the namespace packages
 from .s3Namespace_fire import api as s3
 from .Ec2Namespace_fire import api as ec2
 
 from .IamNamespace_fire import api as iam
 from .Route53Namespace_fire import api as route53
 
-
+##############initialize api object
 api = Api(
     title='AWS API',
     version='1.0',
@@ -14,6 +16,8 @@ api = Api(
    
 )
 
+
+#####add the namespaces with endpoint paths
 api.add_namespace(s3, path='/s3')
 api.add_namespace(ec2, path='/ec2')
 api.add_namespace(iam, path='/iam')
